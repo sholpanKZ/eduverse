2931a56e38cdff29ee293cac02e08901638519a7
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-06-23 12:19:56 +0600

    added new and create to courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,4 +5,18 @@ class CoursesController < ApplicationController
   def show
     @courses = Course.find(params[:id])
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(title: "...", body: "...",video_url:"...")
+
+    if @course.save
+      redirect_to @course
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
