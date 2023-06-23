59c2a50805839790294b5e57e05eb1edb5ca346a
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-06-23 11:58:59 +0600

    added link_to for course to index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/courses", to: "courses#index"
    get "/courses/:id", to: "courses#show"
   get 'home/index'
+   resources :courses
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root  'home#index'
